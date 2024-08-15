b06469837945d02284c8e2ea5221fe0c09ea54a7
Yasmin Xavier <yasminxavier@MacBook-Pro-de-Yasmin.local>
2024-08-15 15:43:03 -0300

    form creation

--- learn_angular/src/app/details/details.component.ts
+++ learn_angular/src/app/details/details.component.ts
@@ -1,43 +1,76 @@
-import {Component, inject} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ActivatedRoute} from '@angular/router';
-import {HousingService} from '../housing.service';
-import {Housinglocation} from '../housinglocation';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <article>
-      <img
-        class="listing-photo"
-        [src]="housinglocation?.photo"
-        alt="Exterior photo of {{ housinglocation?.name }}"
-        crossorigin
-      />
-      <section class="listing-description">
-        <h2 class="listing-heading">{{ housinglocation?.name }}</h2>
-        <p class="listing-location">{{ housinglocation?.city }}, {{ housinglocation?.state }}</p>
-      </section>
-      <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
-        <ul>
-          <li>Units available: {{ housinglocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housinglocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housinglocation?.laundry }}</li>
-          <!-- Notice that the housingLocation properties are being accessed with the optional chaining 
-           operator ?. This ensures that if the housingLocation value is null or undefined the application 
-           doesn't crash. -->
-        </ul>
-      </section>
-    </article>
-  `,
+  <article>
+    <img
+      class="listing-photo"
+      [src]="housinglocation?.photo"
+      alt="Exterior photo of {{ housinglocation?.name }}"
+      crossorigin
+    />
+    <section class="listing-description">
+      <h2 class="listing-heading">{{ housinglocation?.name }}</h2>
+      <p class="listing-location">{{ housinglocation?.city }}, {{ housinglocation?.state }}</p>
+    </section>
+
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{ housinglocation?.availableUnits }}</li>
+        <li>Does this location have wifi: {{ housinglocation?.wifi }}</li>
+        <li>Does this location have laundry: {{ housinglocation?.laundry }}</li>
+      </ul>
+    </section>
+
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+
+        <label for="first-name">First Name</label>
+        <input id="first-name" type="text" formControlName="firstName" />
+
+        <label for="last-name">Last Name</label>
+        <input id="last-name" type="text" formControlName="lastName" />
+
+        <label for="email">Email</label>
+        <input id="email" type="email" formControlName="email" />
+        <button type="submit" class="primary">Apply now</button>
+      </form>
+    </section>
+  </article>
+`,
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housinglocation: Housinglocation | undefined;
+  /* This code gives the DetailsComponent access to the 
+ActivatedRoute router feature that enables you to have access 
+to the data about the current route. In the constructor, the 
+code converts the id parameter acquired from the route from a 
+string to a number. 
+http://localhost:4200/details/0 */
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    /* In Angular, FormGroup and FormControl are types that enable you 
+    to build forms. The FormControl type can provide a default value and 
+    shape the form data. In this example firstName is a string and the 
+    default value is empty string.*/
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housinglocation = this.housingService.getHousingLocationById(housingLocationId);
@@ -46,10 +79,17 @@ export class DetailsComponent {
     route parameter as an argument to the getHousingLocationById service function.*/
   }
 
-  /* This code gives the DetailsComponent access to the 
-  ActivatedRoute router feature that enables you to have access 
-  to the data about the current route. In the constructor, the 
-  code converts the id parameter acquired from the route from a 
-  string to a number. 
-  http://localhost:4200/details/0 */
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+      /* The template now includes an event handler 
+      (submit)="submitApplication()". Angular uses parentheses syntax around 
+      the event name to define events in the template code. The code on the 
+      right hand side of the equals sign is the code that should be executed 
+      when this event is triggered*/
+  }
+
 }
--- learn_angular/src/app/housing.service.ts
+++ learn_angular/src/app/housing.service.ts
@@ -98,5 +98,13 @@ export class HousingService {
   }
   /* these functions return either a specific HousingLocation by id 
   or the entire list. */
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+    /* your app's service that receives the form data to send to the data's destination. In this example, 
+    the method writes the data from the form to the browser's console log.*/
+  }
   constructor() { }
 }
